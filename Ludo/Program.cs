e74fb7fb200c6e7f29946d542c940ec6ecd5fc16
Minik Gaarde Lambrecht <thebluechamp@hotmail.com>
2017-09-06 10:26:11 +0200

    Add project files.

---
+++ Ludo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ludo
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Create a new instance of Game class & start a new game of Ludo
+            Game Ludo = new Game();
+        }
+    }
+}
