9419bb0dc41e7cfa70209f1e8754b01de9725098
Minik Gaarde Lambrecht <thebluechamp@hotmail.com>
2017-09-08 08:26:06 +0200

    Updates In Text

--- Ludo/Game.cs
+++ Ludo/Game.cs
@@ -15,15 +15,12 @@ namespace Ludo
         public Game()
         {
             MainMenu();
-        }
-
-        
+        }        
 
         // Create MainMenu
         private void MainMenu()
         {
-            string m1 = "*Welcome to Ludo*" + "\n" + "*****************";
-            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (m1.Length / 2)) + "}", m1));
+            Console.WriteLine("Welcome to Ludo");
             SetNumberOfPlayers();
             CreatePlayers();
             ShowPlayers();            
